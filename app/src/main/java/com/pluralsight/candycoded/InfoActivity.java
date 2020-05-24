1f7c9a84cd87052bd7fbf84748536eeb186bc9fe
parmeshtoyou <parmeshtoyou@gmail.com>
2020-05-24 10:11:11 +0530

    task3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,6 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
     public void createMapIntent(View view) {
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -38,6 +37,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     // ***
-    // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view) {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0123456789")));
+    }
     // ***
 }
